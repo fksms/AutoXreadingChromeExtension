b3c6d6516e5a2681656d077036f795116b5e96cb
daianjibetu <shogo3681@gmail.com>
2020-01-16 19:47:06 +0900

    Update solve.js

--- scripts/solve.js
+++ scripts/solve.js
@@ -22,26 +22,27 @@ function tab_close(winObj) {
 }
 
 //問題自動解答
-async function question_solver(winObj) {
-    var radio_xpath;
-    var next_xpath;
+function question_solver(winObj) {
+    var radio_button;
+    var next_button;
 
-    for (var j = 0; j < 5; j++) {
-        radio_xpath = document.getElementById('quest_answer_' + String(j + 1));
-        radio_xpath.click();
-        for (var i = 0; i < winObj.length; i++) {
-            radio_xpath = winObj[i].document.getElementById('quest_answer_' + String(j + 1));
-            radio_xpath.click();
-        }
-        next_xpath = document.getElementById('btn_next_' + String(j + 1));
-        next_xpath.firstElementChild.firstElementChild.firstElementChild.click();
-        for (var i = 0; i < winObj.length; i++) {
-            next_xpath = winObj[i].document.getElementById('btn_next_' + String(j + 1));
-            next_xpath.firstElementChild.firstElementChild.firstElementChild.click();
-        }
-        await sleep(300);
-    }
+    return new Promise(async (resolve, reject) => {
 
+        for (var j = 0; j < 5; j++) {
+            for (var i = 0; i < winObj.length; i++) {
+                radio_button = winObj[i].document.getElementById('quest_answer_' + String(j + 1));
+                radio_button.click();
+            }
+            await sleep(500);
+            for (var i = 0; i < winObj.length; i++) {
+                next_button = winObj[i].document.getElementById('btn_next_' + String(j + 1));
+                next_button.firstElementChild.firstElementChild.firstElementChild.click();
+            }
+            await sleep(500);
+        }
+        resolve();
+
+    });
 }
 
 //main関数
@@ -50,10 +51,13 @@ async function solve(tabs) {
 
     var winObj = tab_duplication(url, tabs);
     await sleep(5000);
-    question_solver(winObj);
-    await sleep(2000);
+    await question_solver(winObj);
+    winObj = tab_duplication("", tabs); //そのままだとタブを消せないので空タブで上書き
+    await sleep(500);
     tab_close(winObj);
+    await sleep(500);
+    location.href = 'https://xreading.com/books/book_index'
 }
 
-//var tabs = 9; //開くタブの数
+//var tabs = 10; //開くタブの数
 solve(tabs);
